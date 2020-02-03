2d7e5cfe679c45bfa2a54ed183618b323c934c4d
rendrap <rendrapradana@gmail.com>
2020-02-03 10:36:12 +0700

    instance var

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,9 +3,12 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    render('index')
   end
 
   def hello
+    @array = [1,2,3,4,5]
+    render('hello')
   end
 
   def other_hello
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   root 'demo#index'
 
+  get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
   get 'demo/lynda'
