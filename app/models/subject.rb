3659ad3703acdfd3abec84fdb9bcd17d403aee76
rendrap <rendrapradana@gmail.com>
2020-03-22 21:22:40 +0700

    create 1:1 association for Page

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,7 @@
 class Subject < ApplicationRecord
+
+  has_one :page
+
   scope :visible, lambda { where(:visible => true) }
   scope :invisible, lambda { where(:visible => false) }
   scope :sorted, lambda { order("position ASC") }
