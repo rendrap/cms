512e8ad7a8582491352202498c595b20f2e37799
rendrap <rendrapradana@gmail.com>
2020-03-24 18:01:30 +0700

    Add Subjects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
   # simple routes
   # get 'demo/index'
 
