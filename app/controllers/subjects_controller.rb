bf58d7c039aa5fe04a79ab345d4b6c0f7832b265
rendrap <rendrapradana@gmail.com>
2020-03-26 21:59:54 +0700

    add new form action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,10 +9,19 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new
   end
 
   def create
-
+    # intantiate new object using form  para
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # redirect to index action
+      redirect_to(subjects_path)
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -26,6 +35,11 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
 
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
