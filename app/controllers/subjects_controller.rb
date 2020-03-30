0228282b7e78406ca1891ea49cc4cd827e43cfb8
rendrap <rendrapradana@gmail.com>
2020-03-30 16:01:31 +0700

    Adding delete action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -20,7 +20,7 @@ class SubjectsController < ApplicationController
       # redirect to index action
       redirect_to(subjects_path)
     else
-      render('new')
+      render('edit')
     end
   end
 
@@ -29,13 +29,25 @@ class SubjectsController < ApplicationController
   end
 
   def update
-
+    # Find a new object using form  para
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # redirect to show action
+      redirect_to(subjects_path(@subject))
+    else
+      render('new')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    redirect_to(subjects_path)
   end
 
   private
