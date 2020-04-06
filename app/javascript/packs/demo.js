950db3b694587008a62b151700ae9a1896d2821c
rendrap <rendrapradana@gmail.com>
2020-04-06 09:05:37 +0700

    adding javascipt via webpacker pack pipeline

---
+++ app/javascript/packs/demo.js
@@ -0,0 +1,19 @@
+// document.addEventListener("turbolinks:load", function() {
+
+  // var roar = function roar(name) {
+  //   document.getElementById("roar_id").onclick = function() {
+  //   alert('I am ' + name + '. Hear me roar!');
+  //   }
+  // }
+
+
+// })
+
+
+
+window.roar = function(name) {
+  alert('I am ' + name + '. Hear me roar!');
+  // alert("hello from inside func");
+}
+//   console.log("hhh");
+
