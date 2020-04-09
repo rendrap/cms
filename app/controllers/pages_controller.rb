1dc73ead08faa02babb4a9b26a9a718239646cc1
rendrap <rendrapradana@gmail.com>
2020-04-09 14:50:04 +0700

    adding custom action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
 
   layout 'admin'
+
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
+
   def index
+    logger.debug("*** Testing the logger ***")
     @pages = Page.sorted
   end
 
@@ -59,4 +64,16 @@ class PagesController < ApplicationController
   def page_params
     params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
   end
+
+  def find_subjects
+    @subject = Subject.sorted
+  end
+
+  def set_page_count
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] =='create'
+      @page_count += 1
+    end
+  end
+
 end
